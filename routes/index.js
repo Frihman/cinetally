1c319d3cfb6ec76ac10c71c3b4ec32da238ee24a
James Frih <jamesfrih@me.com>
2021-05-02 14:31:52 +0200

    started on the login page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cinetally - Log In' });
+  res.render('login', { title: 'Cinetally - Log In' });
 });
 
 module.exports = router;
