cc7a4a2af18ad3d70449ed5005a1e8acdaae937f
James Frih <jamesfrih@me.com>
2021-05-06 13:47:06 +0200

    added som css to index page and a session destroy when logging out

--- routes/logOut.js
+++ routes/logOut.js
@@ -1,10 +1,11 @@
 var express = require('express');
+const session = require('express-session');
 var router = express.Router();
 
 /* GET logout */
 router.get('/', function(req, res, next) {
     if (req.session.loggedIn == true) {
-        req.session.loggedIn = false;
+        req.session.destroy();
         res.redirect('/loginpage');
     } else {
         res.redirect('/loginpage');
