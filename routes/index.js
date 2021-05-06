ed439c86163e1e0126fe2e0fcef718c5a359c72b
James Frih <jamesfrih@me.com>
2021-05-06 14:08:02 +0200

    added email to header and search bar to adder. Along with css changes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (req.session.loggedIn == true) {
-    res.render('index', { title: 'Cinetally - Home' });
+    res.render('index', { title: 'Cinetally - Home', email: req.session.Email});
   } else {
     res.redirect('/loginpage');
   }
