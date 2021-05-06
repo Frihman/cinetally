06c9b83c47b66be28529292fa957d537be47cacd
James Frih <jamesfrih@me.com>
2021-05-06 19:06:28 +0200

    structure of search page and css changes

--- routes/search.js
+++ routes/search.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https');
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
+router.get('/:query', function(req, res, next) {
+    
     if (req.session.loggedIn == true) {
-        req.session.searchData = [];
+       
         res.render('search', { title: 'Search', email: req.session.Email});
     } else {
         res.redirect('/loginpage');
