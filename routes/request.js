0e6ad75032bada8d86b847c401a6c8647af0718b
James Frih <jamesfrih@me.com>
2021-05-06 13:58:17 +0200

    css fixes and userdata is stored in session

--- routes/request.js
+++ routes/request.js
@@ -13,10 +13,13 @@ router.get('/users', function(req, res, next) {
 
 router.post('/login', function(req, res, next) {
   mySQLQuery(`SELECT * FROM User WHERE Email = '${req.body.Email}'`, function(result) {
+    var data = result[0];
     if (result.length > 0) {
       bcrypt.compare(req.body.Password, result[0].Password, function(err, result) {
         if (result == true) {
           req.session.loggedIn = true;
+          req.session.Id = data.Id;
+          req.session.Email = data.Email;
           res.redirect('/');
         } else {
           res.redirect('/loginpage');
@@ -31,12 +34,15 @@ router.post('/login', function(req, res, next) {
 
 router.post('/users', function(req, res, next) {
   mySQLQuery(`SELECT * FROM User WHERE Email = '${req.body.Email}'`, function(result) {
+    var data = result[0];
     if (result.length < 1) {
       
       bcrypt.genSalt(saltRounds, function(err, salt) {
         bcrypt.hash(req.body.Password, salt, function(err, hash) {
             mySQLQuery(`INSERT INTO User (Email, Password) VALUES ('${req.body.Email}', '${hash}')`, function(result) {
               req.session.loggedIn = true;
+              req.session.Id = data.Id;
+              req.session.Email = data.Email;
               res.redirect('/');
             });
         });
